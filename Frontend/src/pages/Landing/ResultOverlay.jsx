53b35b37f17522a3f9b9a2bc0e021b19a85a948f
Egba Nnaemeka <egba.nnaemeka@nvit.tech>
2023-10-26 17:57:37 +0100

    Phish sense

---
+++ Frontend/src/pages/Landing/ResultOverlay.jsx
@@ -0,0 +1,8 @@
+const ResultOverlay = ({ res }) => {
+    return (
+        <div className="absolute h-screen z-40 w-full bg-white/[.7] top-0 left-0">
+
+        </div>
+    )
+}
+export default ResultOverlay;
